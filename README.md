800ad55a0f7f40c4ef85dbdd8ece8eed2775b47e
celedev97 <celedev97@gmail.com>
2022-01-06 15:40:13 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 
 # [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+
+A little project for the FreeCodeCamp - [Back End Development and APIs](https://www.freecodecamp.org/learn/back-end-development-and-apis) certification.
+
+Created from the [FreeCodeCamp boilerplate](https://github.com/freeCodeCamp/boilerplate-project-timestamp/)
+
+API Code by [celedev97](https://github.com/celedev97)
--- server.js
+++ server.js
@@ -1,29 +1,42 @@
-// server.js
-// where your node app starts
-
-// init project
+// init express
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enable CORS so that my API is testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
-// http://expressjs.com/en/starter/static-files.html
+// static files (used only for CSS)
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// route for the homepage
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//route for the timestamp or date api
+app.get("/api/:timestampOrDate", (req, res) => {
+  let input = req.params.timestampOrDate
 
-// your first API endpoint... 
+  //checking if the input is numeric
+  if(/^[0-9]*$/.test(input)){
+    //if it is then it's a timestamp and I should convert it to a number
+    input = parseInt(input, 10)
+  }
+
+  //converting it to a date and sending the result as a json
+  let date = new Date(input)
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  })
+})
+
+
+// this is totally useless, but maybe FCC requires it since it was in the boilerplate... (?)
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
 
 
 // listen for requests :)
