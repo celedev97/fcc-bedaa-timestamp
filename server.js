cd54653b95503402fc0ca8c1be28f0072696faa0
celedev97 <celedev97@gmail.com>
2022-01-06 16:42:05 +0100

    server.js mini refactoring

--- server.js
+++ server.js
@@ -1,49 +1,36 @@
-// init express
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-// enable CORS so that my API is testable by FCC
-var cors = require('cors');
+const {stringToDate, dateToJson} = require('./helpers.js')
+
+//#region server setup
+//enable CORS so that my API is testable by FCC
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));
 
 // static files (used only for CSS)
 app.use(express.static('public'));
+//#endregion
 
+//#region routes
 // route for the homepage
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-//route for the timestamp or date api
-app.get("/api/:timestampOrDate", (req, res) => {
-  let input = req.params.timestampOrDate
-
-  //checking if the input is numeric
-  if(/^[0-9]*$/.test(input)){
-    //if it is then it's a timestamp and I should convert it to a number
-    input = parseInt(input, 10)
-  }
-
-  //converting it to a date and sending the result as a json
-  let date = new Date(input)
-  if(isNaN(date.getTime())){
-    res.json({ error : "Invalid Date" })
-  }else{
-    res.json({
-      unix: date.getTime(),
-      utc: date.toUTCString(),
-    })
-  }
+app.get("/api/(:timestampOrDate)", (req, res) => {
+  res.json(dateToJson(stringToDate(req.params['timestampOrDate'])))
 })
 
-
+/*
 // this is totally useless, but maybe FCC requires it since it was in the boilerplate... (?)
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
+*/
+//#endregion
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
